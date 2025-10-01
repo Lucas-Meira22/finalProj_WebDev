8b74ee9c64e48df6815800977657abcbec53e81f
LucasMeira22 <lucasomeira@gmail.com>
2025-09-30 20:24:40 -0400

    Add: Functionalities and Buttons Logic

--- personalFinacialTrack/personalFinacialTrack/NewGoalPage.xaml.cs
+++ personalFinacialTrack/personalFinacialTrack/NewGoalPage.xaml.cs
@@ -1,9 +1,42 @@
-namespace personalFinacialTrack;
+using personalFinacialTrack.Resources.Model;
 
+namespace personalFinacialTrack;
 public partial class NewGoalPage : ContentPage
 {
-	public NewGoalPage()
-	{
-		InitializeComponent();
-	}
+    private string _selectedColor;
+    public NewGoalPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnCancelClicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
+    private void OnColorSelected(object sender, EventArgs e)
+    {
+        if (sender is Button btn && btn.CommandParameter != null)
+        {
+            _selectedColor = btn.CommandParameter.ToString();
+        }
+    }
+    private async void OnSaveClicked(object sender, EventArgs e)
+    {
+        //TODO NAME OF FIELDS
+        var goal = new Goal
+        {
+            //Name = GoalName.text,
+            //Amount = decimal.TryParse(GoalAmountEntry.Text, out var amount) ? amount : 0,
+            //Currency = CurrencyPicker.SelectedItem?.ToString(),
+            //Deadline = DeadlinePicker.Date, // can be optional, handle null if needed
+            //Note = GoalNoteEditor.Text,
+            //Color = _selectedColor
+        };
+        await DisplayAlert("Goal Saved",
+            $"Name: {goal.Name}\nAmount: {goal.Amount}\nCurrency: {goal.Currency}\nDeadline: {goal.Deadline}\nNote: {goal.Note}\nColor: {goal.Color}",
+            "OK");
+
+        // TODO: Save to database, API, or pass back to previous page
+        await Navigation.PopAsync();
+    }
 }
---
+++ personalFinacialTrack/personalFinacialTrack/Resources/Model/Goal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personalFinacialTrack.Resources.Model
+{
+    public class Goal
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public DateTime? Deadline { get; set; }
+        public string Note { get; set; }
+        public string Color { get; set; } // store as hex string like "#FF0000"
+    }
+}
